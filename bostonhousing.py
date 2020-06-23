1ef02eb4600acf5cef8c0aa17fbfb5482b21c6f1
Arpita <arpitamishrarkm@gmail.com>
2020-06-23 16:25:15 +0530

    Update bostonhousing.py

--- bostonhousing.py
+++ bostonhousing.py
@@ -2,16 +2,21 @@ from sklearn import datasets
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-data = datasets.load_boston()
-# define the data/predictors as the pre-set feature names
-df = pd.DataFrame(data.data, columns=data.feature_names)
-# Put the target (housing value -- MEDV) in another DataFrame
-target = pd.DataFrame(data.target, columns=["MEDV"])
-X = df["RM"]
-y = target["MEDV"]
-# Note the difference in argument order
-model = sm.OLS(y, X).fit()
-predictions = model.predict(X) # make the predictions by the model
-# Print out the statistics
-model.summary()
-predictions.to_csv("C:\\Users\\RAJIV MISHRA\\Desktop\\Arpita\\sampleproblemdataset\\housing data\\output.csv")
+def data_laoder_and_predict_function():
+  data = datasets.load_boston()
+  # define the data/predictors as the pre-set feature names
+  df = pd.DataFrame(data.data, columns=data.feature_names)
+  # Put the target (housing value -- MEDV) in another DataFrame
+  target = pd.DataFrame(data.target, columns=["MEDV"])
+  X = df["RM"]
+  y = target["MEDV"]
+  # Note the difference in argument order
+  model = sm.OLS(y, X).fit()
+  predictions = model.predict(X) # make the predictions by the model
+  # Print out the statistics
+  summary = model.summary()
+  print(summary)
+  predictions.to_csv(".\sampleproblemdataset\\housing data\\output.csv")
+  
+  if __name__=="__main__":
+    data_loader_and_predict_function()
